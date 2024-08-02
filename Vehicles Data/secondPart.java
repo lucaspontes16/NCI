e5e39d67d7843805d7c4fcf09781f25967cb1535
Lucas <lucas@pontes.com>
2024-08-02 17:12:38 +0100

    finished CA

---
+++ Vehicles Data/secondPart.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+
+public class exception {
+    public static void main(String[] args) throws FileNotFoundException {
+
+         int id = 0;
+         String sName = "";
+         String sFuel = "";
+         String sLocation = "";
+         int iPostcode = 0;
+         long lValue = (long) 0;
+
+         
+
+    }
+}
